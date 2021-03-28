10823362223b16beeb167c7e835c86c8e3ac5cbf
Rafael <macedo.rafanazareth@gmail.com>
2021-03-27 22:05:52 -0300

    Add destionation API

--- rotes.js
+++ rotes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const routes = express.Router();
+const service = require("./service")
 
 routes.get("/hellow", (req, res) => {
+    return res.send("Hello world");
+  });
+
+  routes.post("/destinos", (req, res) => {
+
     return res.send("Hello world");
   });
 
---
+++ service.js
@@ -0,0 +1,23 @@
+
+const snowJson = require('./mocks/destinations_snow.json')
+
+module.exports = {
+  destinations(preference) {
+    switch (preference) {
+      case "neve":
+        return snowJson;
+
+      case "praia":
+        return snowJson;
+
+      case "cidade":
+        return snowJson;
+      case "montanha":
+        return snowJson;
+
+      default:
+        break;
+    }
+  },
+};
+
